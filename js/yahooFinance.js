7082261997267bd90538179d535c6c2fd4069cfc
tikho <iatikho@gmail.com>
2016-05-04 03:25:56 +0300

    Integrated images and handlebars

--- js/yahooFinance.js
+++ js/yahooFinance.js
@@ -1,41 +1,50 @@
 $(document).ready(function () {
 
-    
+    var companiesSymbols = ['AAPL', 'FB', 'USDRUB=X', 'TSLA'];
 
-    var source   = $("#stock").html();
-    var template = Handlebars.compile(source);
-
-    var html = template(suggestionsWithLetters);
-    $('body').append(html);
+    getData(companiesSymbols);
 
 });
 
-function getData(symbol) {
+getData = function(symbols) {
+
+    var results = [];
+
     var url = 'http://query.yahooapis.com/v1/public/yql';
-    var data = encodeURIComponent("select * from yahoo.finance.quotes where symbol in ('" + symbol + "')");
 
-    $.getJSON(url, 'q=' + data + "&format=json&diagnostics=true&env=http://datatables.org/alltables.env")
-        .done(function (data) {
-            console.log(data);
-            var result = data.query.results.quote;
-            $('#price').text("Price: " + result.LastTradePriceOnly);
-            $('#time').text("Last trade time: " + result.LastTradeTime);
-            $('#change').text(result.Change);
-            $('#percent-change').text(result.PercentChange);
-            if (result.Change > 0){
-                console.log("happy");
-                document.getElementById("image").src= "img/tim-cook-happy.jpg";
-            } else {
-                console.log("sad");
-                document.getElementById("image").src= "img/tim-cook-sad.jpg";
-            }
-        })
-        .fail(function (jqxhr, textStatus, error) {
-            var err = textStatus + ", " + error;
-            console.log('Request failed: ' + err);
-        });
+    for (var i = 0; i < symbols.length; i++) {
+        var data = encodeURIComponent("select * from yahoo.finance.quotes where symbol in ('" + symbols[i] + "')");
+
+        $.getJSON(url, 'q=' + data + "&format=json&diagnostics=true&env=http://datatables.org/alltables.env")
+            .done(function (data) {
+                var result = data.query.results.quote;
+
+                results.push(result);
+
+                if (results.length == symbols.length){
+
+                    results = results.sort(function(a,b){
+                      if (a.Name < b.Name)
+                        return -1;
+                      if (a.Name > b.Name)
+                        return 1;
+                      return 0;
+                    });
+
+                    var source   = $("#stock").html();
+                    var template = Handlebars.compile(source);
+
+                    var html = template(results);
+                    $('body').append(html);
+                    console.log(results);
+                }
+            })
+            .fail(function (jqxhr, textStatus, error) {
+                var err = textStatus + ", " + error;
+                console.log('Request failed: ' + err);
+            });
+    }
 }
-
 
 Handlebars.registerHelper('grouped_each', function(every, context, options) {
   var out = "", subcontext = [], i;
@@ -50,4 +59,14 @@ Handlebars.registerHelper('grouped_each', function(every, context, options) {
       out += options.fn(subcontext);
   }
   return out;
+});
+
+
+Handlebars.registerHelper('image', function(symbol, change) {
+    var imgClass = 'class="image"';
+    if (change > 0){
+        return '<img src="img/' + symbol + "-happy.jpg" + '" ' + imgClass + '>';
+    } else{
+        return '<img src="img/' + symbol + "-sad.jpg" + '" ' + imgClass + '>';
+    }
 });
